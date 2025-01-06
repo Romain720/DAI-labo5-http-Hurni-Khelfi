4eeb3702accb4753b556f1d1b3dc64b976c51807
Romain <romain.hurni@hes-so.ch>
2025-01-06 18:11:34 +0100

    update étape 3

--- labo5-http-code-java/src/main/java/org/example/Main.java
+++ labo5-http-code-java/src/main/java/org/example/Main.java
@@ -16,13 +16,13 @@ public class Main {
             todos.add(todo);
             ctx.status(201);
         });
-        app.put("/todos/:id", ctx -> { // Mettre à jour un élément
+        app.put("/todos/{id}", ctx -> { // Mettre à jour un élément
             int id = Integer.parseInt(ctx.pathParam("id"));
             String updatedTodo = ctx.body();
             todos.set(id, updatedTodo);
             ctx.status(204);
         });
-        app.delete("/todos/:id", ctx -> { // Supprimer un élément
+        app.delete("/todos/{id}", ctx -> { // Supprimer un élément
             int id = Integer.parseInt(ctx.pathParam("id"));
             todos.remove(id);
             ctx.status(204);
